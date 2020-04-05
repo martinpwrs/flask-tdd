9dc3e7f5dd6325479bc33ab5b5b5204566d704ec
Martin Powers <martinpwrs@gmail.com>
2020-04-04 20:35:16 -0600

    green.

--- project/api/users.py
+++ project/api/users.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from flask_restx import Resource, Api
+from flask_restx import Resource, Api, fields
 
 from project import db
 from project.api.models import User
@@ -8,8 +8,16 @@ from project.api.models import User
 users_blueprint = Blueprint('users', __name__)
 api = Api(users_blueprint)
 
+user = api.model("User", {
+    'id': fields.Integer(readOnly=True),
+    'username': fields.String(required=True),
+    'email': fields.String(required=True),
+    'created_date': fields.DateTime,
+})
+
 
 class UsersList(Resource):
+    @api.expect(user, validate=True)
     def post(self):
         post_data = request.get_json()
         username = post_data.get("username")
